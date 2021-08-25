2e76f4a60e202a3fb1e7fd0b62ca30dcbee2183c
Facundo <facusgalante@gmail.com>
2021-08-25 14:41:04 -0300

    socket-client conectado con el servidor

--- models/server.js
+++ models/server.js
@@ -15,6 +15,9 @@ class Server {
 
     // Rutas de mi aplicación
     this.routes();
+
+    //Sockets
+    this.sockets();
   }
 
   middlewares() {
@@ -32,6 +35,15 @@ class Server {
   listen() {
     this.server.listen(this.port, () => {
       console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
+  sockets() {
+    this.io.on("connection", (socket) => {
+      console.log("Cliente conectado", socket.id);
+
+      socket.on("disconnect", () => {
+        console.log("Cliente desconectado", socket.id);
+      });
     });
   }
 }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,17 @@
-console.log("Hola desde index");
+const lblOnline = document.querySelector("#lblOnline");
+const lbloffline = document.querySelector("#lblOffline");
+
+//socket del cliente
+const socket = io();
+
+socket.on("connect", () => {
+  console.log("Conectado");
+  lblOffline.style.display = "none";
+  lblOnline.style.display = "";
+});
+
+socket.on("disconnect", () => {
+  console.log("desconectado");
+  lblOffline.style.display = "";
+  lblOnline.style.display = "none";
+});
