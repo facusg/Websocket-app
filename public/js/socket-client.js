3ee64844b60326d83c39158ad30a6d185668f415
Facundo <facusgalante@gmail.com>
2021-08-25 19:54:52 -0300

    Enviar mensaje desde el cliente al backend terminado

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,5 +1,7 @@
 const lblOnline = document.querySelector("#lblOnline");
 const lbloffline = document.querySelector("#lblOffline");
+const txtMensaje = document.querySelector("#txtMensaje");
+const btnEnviar = document.querySelector("#btnEnviar");
 
 //socket del cliente
 const socket = io();
@@ -14,4 +16,15 @@ socket.on("disconnect", () => {
   console.log("desconectado");
   lblOffline.style.display = "";
   lblOnline.style.display = "none";
+});
+
+btnEnviar.addEventListener("click", () => {
+  const mensaje = txtMensaje.value;
+  const payload = {
+    mensaje,
+    id: "123abc",
+    fecha: new Date().getTime(),
+  };
+
+  socket.emit("enviar-mensaje", payload);
 });
